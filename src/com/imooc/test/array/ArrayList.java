51edf85243adae578cd3e4c15b5a504f99c061a9
kk <511158469@qq.com>
2020-10-27 23:37:26 +0800

    第二节课 动态数组、ArrayList自实现及源码分析

--- src/com/imooc/test/array/ArrayList.java
+++ src/com/imooc/test/array/ArrayList.java
@@ -1,14 +1,16 @@
 package com.imooc.test.array;
 
-public class ArrayList {
+
+@SuppressWarnings("unchecked")
+public class ArrayList<E> {
     private int size;
-    private int [] elements;
+    private E [] elements;
     private static final int DEFAULT_CAPACITY=10;
     private static final int ELEMENT_NOT_FOUND=-1;
     public ArrayList(int capacity)
     {
         capacity= Math.max(capacity, DEFAULT_CAPACITY);
-        elements=new int[capacity];
+        elements= (E[]) new Object[capacity];
     }
     public ArrayList()
     {
@@ -20,43 +22,156 @@ public class ArrayList {
     }
     public void clear()
     {
+        //改成object 对象管理细节
+        for (int i = 0; i < size; i++) {
+            elements[i]=null;
+        }
         size=0;
     }
     public boolean isEmpty()
     {
         return size==0;
     }
-    public int get(int index)
+    public E get(int index)
     {
-        if(index<0||index>=size)
-        {
-            throw new IndexOutOfBoundsException("Index:"+index+",Size:"+size);
-        }
+        rangeCheck(index);
         return elements[index];
     }
-    public int set(int index,int element)
+    public E set(int index,E element)
     {
-        if(index<0||index>=size)
-        {
-            throw new IndexOutOfBoundsException("Index:"+index+",Size:"+size);
-        }
-        int old = elements[index];
+        rangeCheck(index);
+        E old = elements[index];
         elements[index]=element;
         return old;
     }
-    public int indexOf(int element)
+    public int indexOf(E element)
     {
-        for (int i = 0; i < size; i++) {
-            if(elements[i]==element) return i;
+        //空值的处理
+        if(element==null)
+        {
+            for (int i = 0; i < size; i++) {
+                //equal默认比较内存地址 如果重新了equal 方法 就用重写的方法比较
+                //element为空 意味着 elements[i]找到空就行
+                if(elements[i]==null) return i;
+            }
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                //equal默认比较内存地址 如果重新了equal 方法 就用重写的方法比较
+                //这里注意elements[i]可能会空指针
+                if(element.equals(elements[i])) return i;
+
+            }
         }
         return ELEMENT_NOT_FOUND;
     }
-    public boolean contains(int element)
+    public boolean contains(E element)
     {
         return indexOf(element)!=ELEMENT_NOT_FOUND;
     }
+    public void add(E element)
+    {
+        /*elements[size]=element;
+        size++;*/
+        //elements[size++]=element;
+        add(size,element);
+    }
+    public E remove(int index)
+    {
+        rangeCheck(index);
+        E old = elements[index];
+        for (int i = index+1; i <size; i++) {
+            elements[i-1]=elements[i];
+        }
+//        size--;
+//        elements[size]=null;
+        //最后一个设为null
+        elements[--size]=null;
+        return old;
+    }
+
+
+
+    //注意 这里必须从大的位置开始挪动 如果从小的地方开始挪动 就会都被小的值给替换掉
+    public void add(int index,E element)
+    {
+        rangeCheckForAdd(index);
+        //至少保证size+1
+        ensureCapacity(size+1);
+
+        for (int i = size-1; i >=index; i--) {
+            elements[i+1]= elements[i];
+        }
+        elements[index]=element;
+        size++;
+    }
+
+
+    /**
+     * 扩容操作
+     * @param capacity
+     */
+    private void ensureCapacity(int capacity) {
+        //并不是每次都需要扩容
+        int oldCapacity = elements.length;
+        if(oldCapacity>=capacity) return;
+        //扩容操作
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        E [] newElements= (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {  //这里的循环次数用size和oldCapacity都可以
+            newElements[i]=elements[i];
+        }
+        elements=newElements;
+
+    }
+
+    private void rangeCheck(int index) {
+        if(index<0||index>=size)
+        {
+            OutOfBounds(index);
+        }
+    }
+    private void rangeCheckForAdd(int index) {
+        if(index<0||index>size)
+        {
+            OutOfBounds(index);
+        }
+    }
+
+    private void OutOfBounds(int index) {
+        throw new IndexOutOfBoundsException("Index:" + index + ",Size:" + size);
+    }
+
+    @Override
+    public String toString() {
+       StringBuilder stringBuilder=new StringBuilder();
+       stringBuilder.append("size=").append(size).append(", elements=[");
+        for (int i = 0; i < size; i++) {
+            if(i!=0)
+            {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append(elements[i]);
+        }
+       stringBuilder.append("]");
+       return stringBuilder.toString();
+    }
+
     public static void main(String[] args) {
-        ArrayList list=new ArrayList();
-        list.get(-10);
+        ArrayList<Integer> list=new ArrayList<>();
+//        list.add(0,120);
+//        for (int i = 1; i <= 10; i++) {
+//            list.add(i);
+//        }
+//        System.out.println(list);
+        list.add(null);
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+        System.out.println(list.indexOf(1));
+        //list.add(list.size(),120);
+        //list.remove(8);
+        //System.out.println(list);
+
     }
 }
--- src/com/imooc/test/feibo/Fibo.java
+++ src/com/imooc/test/feibo/Fibo.java
@@ -3,6 +3,7 @@ package com.imooc.test.feibo;
 public class Fibo {
     // 0 1 2 3 4 5 6 7  8
     // 0 1 1 2 3 5 8 13 21
+    //还可以用斐波那契数学公式求解
     public static int fibo(int n)
     {
         if(n<=1) return n;
@@ -20,6 +21,18 @@ public class Fibo {
         }
         return second;
     }
+    public static int fibo3(int n)
+    {
+        if(n<=1) return n;
+        int first=0;
+        int second=1;
+        while (n-->1) {
+            //n-->1 执行n-1次  n-->0 执行n次
+            second+=first;
+            first=second-first;
+        }
+        return second;
+    }
     public static int sum1(int n)
     {
         int result=0;
@@ -33,6 +46,7 @@ public class Fibo {
         return (1+n)*n/2;
     }
     public static void main(String[] args) {
+        //System.out.println(fibo3(8));
         /*for (int i = 0; i < 10; i++) {
             System.out.print(fibo(i)+" ");
         }*/
