0b7215e71a589af7ac9e6cae98ed0d3d2f7be1b5
kk <511158469@qq.com>
2020-11-15 20:43:56 +0800

    add  第四节课 双向链表 LinkedList自实现及源码分析

--- src/com/imooc/test/array/ArrayList2.java
+++ src/com/imooc/test/array/ArrayList2.java
@@ -5,70 +5,73 @@ import com.imooc.test.linked.AbstractList;
 
 /**
  * 有缩容操作
+ *
  * @param <E>
  */
 @SuppressWarnings("unchecked")
 public class ArrayList2<E> extends AbstractList<E> {
 
-    private E [] elements;
-    private static final int DEFAULT_CAPACITY=10;
-    public ArrayList2(int capacity)
-    {
-        capacity= Math.max(capacity, DEFAULT_CAPACITY);
-        elements= (E[]) new Object[capacity];
+    private E[] elements;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public ArrayList2(int capacity) {
+        capacity = Math.max(capacity, DEFAULT_CAPACITY);
+        elements = (E[]) new Object[capacity];
     }
-    public ArrayList2()
-    {
+
+    public ArrayList2() {
         this(DEFAULT_CAPACITY);
     }
 
-    public void clear()
-    {
+    public void clear() {
         //改成object 对象管理细节
         for (int i = 0; i < size; i++) {
-            elements[i]=null;
+            elements[i] = null;
         }
-        size=0;
+        size = 0;
+        //缩容 仅供参考
+        if (elements != null && elements.length > DEFAULT_CAPACITY) {
+            elements = (E[]) new Object[DEFAULT_CAPACITY];
+        }
     }
 
     /**
      * 复杂度O(1)  随机访问非常快
+     *
      * @param index
      * @return
      */
-    public E get(int index)
-    {
+    public E get(int index) {
         rangeCheck(index);
         return elements[index];
     }
+
     /**
      * 复杂度O(1)  随机访问非常快
+     *
      * @param index
      * @return
      */
-    public E set(int index,E element)
-    {
+    public E set(int index, E element) {
         rangeCheck(index);
         E old = elements[index];
-        elements[index]=element;
+        elements[index] = element;
         return old;
     }
-    public int indexOf(E element)
-    {
+
+    public int indexOf(E element) {
         //空值的处理
-        if(element==null)
-        {
+        if (element == null) {
             for (int i = 0; i < size; i++) {
                 //equal默认比较内存地址 如果重新了equal 方法 就用重写的方法比较
                 //element为空 意味着 elements[i]找到空就行
-                if(elements[i]==null) return i;
+                if (elements[i] == null) return i;
             }
-        }
-        else {
+        } else {
             for (int i = 0; i < size; i++) {
                 //equal默认比较内存地址 如果重新了equal 方法 就用重写的方法比较
                 //这里注意elements[i]可能会空指针
-                if(element.equals(elements[i])) return i;
+                if (element.equals(elements[i])) return i;
 
             }
         }
@@ -76,30 +79,26 @@ public class ArrayList2<E> extends AbstractList<E> {
     }
 
     /**
-     *
      * @param index
-     * @return
-     *   //时间复杂度
-     *     // 最好（ 往最后的位置添加元素 O(1)）
-     *     // 最坏(往最前面的位置添加元素 O(n))
-     *     // 平均  O(n)
+     * @return //时间复杂度
+     * // 最好（ 往最后的位置添加元素 O(1)）
+     * // 最坏(往最前面的位置添加元素 O(n))
+     * // 平均  O(n)
      */
-    public E remove(int index)
-    {
+    public E remove(int index) {
         rangeCheck(index);
         E old = elements[index];
-        for (int i = index+1; i <size; i++) {
-            elements[i-1]=elements[i];
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
         }
 //        size--;
 //        elements[size]=null;
         //最后一个设为null
-        elements[--size]=null;
+        elements[--size] = null;
         //缩容操作
         trim();
         return old;
     }
-
 
 
     //注意 这里必须从大的位置开始挪动 如果从小的地方开始挪动 就会都被小的值给替换掉
@@ -107,20 +106,19 @@ public class ArrayList2<E> extends AbstractList<E> {
     // 最好（ 往最后的位置添加元素 O(1)）
     // 最坏(往最前面的位置添加元素 O(n))
     // 平均  O(n)
-    public void add(int index,E element)
-    {
+    public void add(int index, E element) {
         rangeCheckForAdd(index);
         //至少保证size+1
-        ensureCapacity(size+1);
+        ensureCapacity(size + 1);
 
         /*for (int i = size-1; i >=index; i--) {
             elements[i+1]= elements[i];
         }
         elements[index]=element;*/
-         for (int i = size; i >index; i--) {
-            elements[i]=elements[i-1];  //都是size-1的地方 移动给size
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];  //都是size-1的地方 移动给size
         }
-        elements[index]=element;
+        elements[index] = element;
 
         size++;
     }
@@ -131,57 +129,57 @@ public class ArrayList2<E> extends AbstractList<E> {
      * 复杂度震荡 举例 假设 默认容量为4 当添加第五个元素的时间 需要扩容 为8 添加后 时间复杂度 O（n）
      * 再执行删除操作 缩小到原来的一半 时间复杂度O（n)
      * PS ： 扩容的指数 和缩容的指数 相乘不为1 就不会出现 复杂度震荡问题
+     *
      * @param capacity
      */
     private void ensureCapacity(int capacity) {
         //并不是每次都需要扩容
         int oldCapacity = elements.length;
-        if(oldCapacity>=capacity) return;
+        if (oldCapacity >= capacity) return;
         //扩容操作
         int newCapacity = oldCapacity + (oldCapacity >> 1);
-        E [] newElements= (E[]) new Object[newCapacity];
+        E[] newElements = (E[]) new Object[newCapacity];
         for (int i = 0; i < size; i++) {  //这里的循环次数用size和oldCapacity都可以
-            newElements[i]=elements[i];
+            newElements[i] = elements[i];
         }
-        elements=newElements;
+        elements = newElements;
 
     }
+
     //缩容操作
     private void trim() {
         int capacity = elements.length;
         //新的容量 缩小到原来的一半
-        int newCapacity = capacity>>1;
+        int newCapacity = capacity >> 1;
         // size 大于总容量的1/2 就不需要缩了
         // 而且必须大于默认容量 不然的话 会缩到很小 参考例子 12->6->3->1 然后再扩容 扩起来很麻烦
-        if(size>=(newCapacity) ||capacity<=DEFAULT_CAPACITY) return;
+        if (size >= (newCapacity) || capacity <= DEFAULT_CAPACITY) return;
         //剩余的空间还有很多
-        E [] newElements= (E[]) new Object[newCapacity];
+        E[] newElements = (E[]) new Object[newCapacity];
         for (int i = 0; i < size; i++) {  //这里的循环次数用size和oldCapacity都可以
-            newElements[i]=elements[i];
+            newElements[i] = elements[i];
         }
-        elements=newElements;
+        elements = newElements;
 
     }
-
 
 
     @Override
     public String toString() {
-       StringBuilder stringBuilder=new StringBuilder();
-       stringBuilder.append("size=").append(size).append(", elements=[");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("size=").append(size).append(", elements=[");
         for (int i = 0; i < size; i++) {
-            if(i!=0)
-            {
+            if (i != 0) {
                 stringBuilder.append(",");
             }
             stringBuilder.append(elements[i]);
         }
-       stringBuilder.append("]");
-       return stringBuilder.toString();
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
-        ArrayList2<Integer> list=new ArrayList2<>();
+        ArrayList2<Integer> list = new ArrayList2<>();
 //        list.add(0,120);
 //        for (int i = 1; i <= 10; i++) {
 //            list.add(i);
---
+++ src/com/imooc/test/双向链表/LinkedList.java
@@ -0,0 +1,217 @@
+package com.imooc.test.双向链表;
+
+import com.imooc.test.linked.AbstractList;
+
+/**
+ * gc root 对象 被栈指针（局部变量）指向的对象
+ * 双向链表对比 单向链表 操作数量节省了一半
+ *  总结：
+ *  如果你频繁在尾部添加、删除 元素 双向链表、动态数组都可以
+ *  如果你在首部添加、删除 用双向链表
+ *  如果你在任意位置 添加、删除元素 用双向链表
+ *  频繁查询 用动态数组
+ * @param <E>
+ */
+public class LinkedList<E> extends AbstractList<E> {
+    private Node<E> first;
+    private Node<E> last;
+
+    @Override
+    public void clear() {
+        size = 0;
+        first = null;
+        last = null;
+
+    }
+
+    /**
+     * 时间复杂度  O(n)
+     * 最好 O(1)
+     * 最坏O(n)
+     * 平均O(n)
+     *
+     * @param index
+     * @return
+     */
+    @Override
+    public E get(int index) {
+        return node(index).element;
+    }
+
+    /**
+     * * 时间复杂度  O(n)
+     * * 最好 O(1)
+     * * 最坏O(n)
+     * * 平均O(n)
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    @Override
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
+    }
+
+    /**
+     * * 时间复杂度  O(n)
+     * * 最好 O(1)
+     * * 最坏O(n)
+     * * 平均O(n)
+     * 网上说的链表添加复杂度为O（1） 实际上是指 添加那一时刻 指针的移动为O（1）
+     *
+     * @param index
+     * @param element
+     */
+    @Override
+    public void add(int index, E element) {
+        rangeCheckForAdd(index);
+        if (index == size) //往最后的位置添加元素
+        {
+            Node<E> OldLast = last;//原来的last
+            //往尾部添加 就是把last 指向新添加的节点 然后原来的last的next 为新添加的节点
+            last = new Node<>(OldLast, element, null);
+            //以前的last为空 就是index=0 size=0的情况
+            if (OldLast == null) {
+                first = last;
+            } else {
+                OldLast.next = last;
+            }
+        } else {
+            // 新添加节点的下一个
+            Node<E> next = node(index);// 如果是从0位置插入
+            Node<E> prev = next.prev; //null
+            Node<E> node = new Node<>(prev, element, next);
+            next.prev = node;
+            if (prev == null) { //index==0
+                first = node;
+            } else {
+                prev.next = node;
+            }
+        }
+
+        size++;
+    }
+
+    /**
+     * * 时间复杂度  O(n)
+     * * 最好 O(1)
+     * * 最坏O(n)
+     * * 平均O(n)
+     *
+     * @param index
+     * @return
+     */
+    @Override
+    public E remove(int index) {
+        rangeCheck(index);
+        Node<E> node = node(index);
+        Node<E> next = node.next;
+        Node<E> prev = node.prev;
+        if (prev == null) { //等价于index==0
+            first = next;
+        } else {
+            prev.next = next;
+        }
+        if (next == null) { //等价于 index==size-1
+            last = prev;
+        } else {
+            next.prev = prev;
+        }
+        size--;
+        return node.element;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        //空值的处理
+        if (element == null) {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                //equal默认比较内存地址 如果重新了equal 方法 就用重写的方法比较
+                //element为空 意味着 elements[i]找到空就行
+                if (node.element == null) return i;
+                node = node.next;
+            }
+        } else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                //equal默认比较内存地址 如果重新了equal 方法 就用重写的方法比较
+                //这里注意elements[i]可能会空指针
+                if (element.equals(node.element)) return i;
+                node = node.next;
+
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+           StringBuilder sb=new StringBuilder();
+           if(prev!=null)
+           {
+               sb.append(prev.element);
+           }
+           else {
+               sb.append("null");
+           }
+           sb.append("_").append(element).append("_");
+           if(next!=null)
+           {
+               sb.append(next.element);
+           }
+           else {
+               sb.append("null");
+           }
+           return sb.toString();
+        }
+    }
+
+    private Node<E> node(int index) {
+        rangeCheck(index);
+        if (index < (size >> 1)) {
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        } else {
+            Node<E> node = last;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+            return node;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("size=").append(size).append(", elements=[");
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append(node);
+            node = node.next;
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
