da81d5bc7097ad22d55be0296681f053a1b7de0d
kk <511158469@qq.com>
2020-11-16 16:52:19 +0800

    add 力扣每日一题

---
+++ src/com/imooc/test/每日力扣题目/_1021_删除最外层的括号.java
@@ -0,0 +1,46 @@
+package com.imooc.test.每日力扣题目;
+
+/**
+ * 示例 1：
+ *
+ * 输入："(()())(())"
+ * 输出："()()()"
+ * 解释：
+ * 输入字符串为 "(()())(())"，原语化分解得到 "(()())" + "(())"，
+ * 删除每个部分中的最外层括号后得到 "()()" + "()" = "()()()"。
+ * 示例 2：
+ *
+ * 输入："(()())(())(()(()))"
+ * 输出："()()()()(())"
+ * 解释：
+ * 输入字符串为 "(()())(())(()(()))"，原语化分解得到 "(()())" + "(())" + "(()(()))"，
+ * 删除每个部分中的最外层括号后得到 "()()" + "()" + "()(())" = "()()()()(())"。
+ * 示例 3：
+ *
+ * 输入："()()"
+ * 输出：""
+ * 解释：
+ * 输入字符串为 "()()"，原语化分解得到 "()" + "()"，
+ * 删除每个部分中的最外层括号后得到 "" + "" = ""。
+ *
+ * 链接：https://leetcode-cn.com/problems/remove-outermost-parentheses
+ */
+public class _1021_删除最外层的括号 {
+    public static String removeOuterParentheses(String s) {
+        StringBuilder sb=new StringBuilder();
+        int level=0;
+        for (char c : s.toCharArray()) {
+            if(c==')') level--;
+            if(level>=1) sb.append(c);
+            if(c=='(') level++;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s="()()";
+        System.out.println(removeOuterParentheses(s));
+    }
+
+
+}
---
+++ src/com/imooc/test/每日力扣题目/_1534_统计好三元组.java
@@ -0,0 +1,45 @@
+package com.imooc.test.每日力扣题目;
+
+/**
+ * 0 <= i < j < k < arr.length
+ * |arr[i] - arr[j]| <= a
+ * |arr[j] - arr[k]| <= b
+ * |arr[i] - arr[k]| <= c
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-good-triplets
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 示例 1：
+ * <p>
+ * 输入：arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+ * 输出：4
+ * 解释：一共有 4 个好三元组：[(3,0,1), (3,0,1), (3,1,1), (0,1,1)] 。
+ * 示例 2：
+ * <p>
+ * 输入：arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+ * 输出：0
+ * 解释：不存在满足所有条件的三元组。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/count-good-triplets
+ */
+public class _1534_统计好三元组 {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int result = 0;
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a) { //第三个循环的时候 判断下
+                    for (int k = j + 1; k < arr.length; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                            result++;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
